a2f62229458b5782c1acac1e667ab17c5d0480de
Nigel John <mytrini@att.net>
2016-09-10 11:09:23 -0400

    trying this

--- app.js
+++ app.js
@@ -52,8 +52,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'app_client')));
+app.use(express.static(path.join(__dirname +'/public')));
+app.use(express.static(path.join(__dirname + '/app_client')));
 
 //app.use('/', routes);
 app.use('/api', routesApi);
